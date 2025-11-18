1dc99d1e15c35cba82a5dc17af90802982f0cf63
hans67khmt-oss <ha.ns.67khmt@ntu.edu.vn>
2025-11-18 15:24:50 +0700

    Create Nhap-Thu.py

---
+++ Nhap-Thu.py
@@ -0,0 +1,9 @@
+a=int(input("nhập thứ bạn muốn bằng Số: "))
+if 2<= a <= 8:
+      if a ==8:
+        print("chủ nhật")
+      else:
+        print("thứ", a)
+else:
+      print("không thỏa")
+       
