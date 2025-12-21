e0e7dd87471b53318b3339f462a123f15eaa4b75
hans67khmt-oss <ha.ns.67khmt@ntu.edu.vn>
2025-12-21 19:36:10 +0700

    Create TH15_Bai2_IF.py

---
+++ TH15_Bai2_IF.py
@@ -0,0 +1,9 @@
+a=int(input("nhập số a: "))
+if a%2==0:
+    if a%3==0:
+        print("số đó là số chẵn chia hết cho 3")
+    else:
+        print("số đó là số chẵn và không chia hết cho 3")
+else:
+    print("số đó không phải là số chẵn")
+
