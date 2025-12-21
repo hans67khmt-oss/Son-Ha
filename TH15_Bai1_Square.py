6803157eba3e90e4d64a940d18a4e341b93410a9
hans67khmt-oss <ha.ns.67khmt@ntu.edu.vn>
2025-12-21 19:36:13 +0700

    Create TH15_Bai1_Square.py

---
+++ TH15_Bai1_Square.py
@@ -0,0 +1,6 @@
+a=int(input("nhập chiều dài hình chữ nhật: "))
+b=int(input("nhập chiều rộng hình chữ nhật: "))
+p= (a+b)*2
+s= a*b
+print("diện tích hình chữ nhật là: ",s)
+print("chu vi hình chữ nhật là: ",p)
