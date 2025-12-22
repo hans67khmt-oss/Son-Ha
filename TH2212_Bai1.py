28016b3a69b0362887f0d1be29ba4866f2f4d0be
hans67khmt-oss <ha.ns.67khmt@ntu.edu.vn>
2025-12-22 14:05:39 +0700

    Create TH2212_Bai1.py

---
+++ TH2212_Bai1.py
@@ -0,0 +1,6 @@
+w= float(input("nhap chieu dai: "))
+h= float(input("nhap chieu rong: "))
+p=(h+w)*2
+s=w*h
+print(f"dien tich hinh chu nhat la: {p:.2f}")
+print(f"chu vi hinh chu nhat la: {s:.2f}")
