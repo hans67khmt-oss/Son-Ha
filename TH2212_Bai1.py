8efa76e0b5d3f774597feff36648a582a8c7cfe2
hans67khmt-oss <ha.ns.67khmt@ntu.edu.vn>
2025-12-22 14:14:14 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -1,6 +1,11 @@
-w= float(input("nhap chieu dai: "))
-h= float(input("nhap chieu rong: "))
-p=(h+w)*2
-s=w*h
-print(f"dien tich hinh chu nhat la: {p:.2f}")
-print(f"chu vi hinh chu nhat la: {s:.2f}")
+w= float(input("nhap canh w: "))
+h= float(input("nhap canh h: "))
+while w<0.0 or h>100.0:
+    print("nhap lai!")
+    w= float(input("nhap canh w: "))
+    h= float(input("nhap canh h: "))
+if w>=0.0 and h<=100.0:    
+  p=(h+w)*2
+  s=w*h
+  print(f"dien tich hinh chu nhat la: {p:.2f}")
+  print(f"chu vi hinh chu nhat la: {s:.2f}")
